fc349f98c23b5d42833204990b6a3b4c3c2ef490
CarlosSouza87 <carlosfernandos1000@gmail.com>
2024-04-11 01:49:31 +0000

    ultima1

--- src/main/java/br/gov/sp/fatec/springbootapp/Controller/TrabalhoController.java
+++ src/main/java/br/gov/sp/fatec/springbootapp/Controller/TrabalhoController.java
@@ -4,21 +4,25 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import br.gov.sp.fatec.springbootapp.entity.Trabalho;
 import br.gov.sp.fatec.springbootapp.services.TrabalhoService;
 
+@CrossOrigin
 @RestController
 @RequestMapping(value="/trabalho")
 public class TrabalhoController {
     @Autowired
     TrabalhoService trabalhoService;
 
-    @GetMapping
-    public ResponseEntity<List<Trabalho>> listatrabalho(String titulo,Integer nota){
+    @GetMapping(value="/{titulo}/{nota}")
+    public ResponseEntity<List<Trabalho>> listatrabalho(@PathVariable("titulo")String titulo,
+                                                        @PathVariable("nota")Integer nota){
         List<Trabalho> listTrabalho=trabalhoService.buscaTrabalhoPorTituloNota(titulo, nota);
 
         return ResponseEntity.ok().body(listTrabalho);
