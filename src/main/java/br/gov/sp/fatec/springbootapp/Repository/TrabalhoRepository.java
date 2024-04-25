0e6fc8ba685dfee25a3844652bff7a0394d0fda7
CarlosSouza87 <carlosfernandos1000@gmail.com>
2024-04-25 01:55:04 +0000

    prova

--- src/main/java/br/gov/sp/fatec/springbootapp/Repository/TrabalhoRepository.java
+++ src/main/java/br/gov/sp/fatec/springbootapp/Repository/TrabalhoRepository.java
@@ -7,7 +7,7 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import br.gov.sp.fatec.springbootapp.entity.Trabalho;
 
 public interface TrabalhoRepository extends JpaRepository<Trabalho, Long>{
-    // select  trabalho from tra_trabalho where tra_titulo="banana" and tra_nota >0
+    
 
     List<Trabalho> findByTituloAndNotaGreaterThan(String titulo, Integer nota );
 
